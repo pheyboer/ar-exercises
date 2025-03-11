f7b0efb5c26124ffbe8984f54594822c0b66652c
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-11 14:08:38 -0400

    creating new employees for stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -19,13 +19,14 @@ puts "Average revenue for all stores $#{average_revenue}"
 # output number of stores that generate 1 mil or more 
 
 
-# This does not work**
-# @mil_store = Store.where("annual_revenue > ?", 1000000)
+# This works too
+@mil_store = Store.where("annual_revenue > ?", 1000000)
 
-# @mil_store.each do |store|
-#   puts "Number of stores that make more than 1 million in annual revenue: #{@mil_store}"
-# end
+@mil_store.each do |store|
+  puts "Number of stores that make more than 1 million in annual revenue: #{@mil_store.count}"
+puts "Store: #{store.name} - Annual Revenue: #{store.annual_revenue}"
+end
 
-
-mil_store = Store.where("annual_revenue >= ?", 1000000).size
-puts "Stores that generate over 1 million in revenue: #{mil_store}"
+# Both of these work
+# mil_store = Store.where("annual_revenue >= ?", 1000000).size
+# puts "Stores that generate over 1 million in revenue: #{mil_store}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,23 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# one to many associatoin
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Emplyoee < ActiveRecord::Base
+  belongs_to :store
+end
+
+# Create employees for store 1
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 55)
+@store1.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 50)
+
+# Create employees for store 2
+@store2.employees.create(first_name: "Alex", last_name: "Johnson", hourly_rate: 70)
+@store2.employees.create(first_name: "Sara", last_name: "Lee", hourly_rate: 65)
+@store2.employees.create(first_name: "Emily", last_name: "Davis", hourly_rate: 60)
