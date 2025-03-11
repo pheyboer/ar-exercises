31bcc4be9b30c8a162a1299205d851e32db7a3fc
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-11 13:00:32 -0400

    exercise 2 - assign instance variables, and updating

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Assigning instance variables
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store3 = Store.find(3)
+
+# Update store1
+@store1.update(name: "Brand New Burnaby")
+puts @store1.name
