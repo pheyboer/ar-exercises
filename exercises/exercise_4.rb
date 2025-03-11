ca21ca058846d0903f6ffe83f59d819a02bce256
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-11 13:22:10 -0400

    fetching womens stores with annual revenue less than 1 mil

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -20,7 +20,7 @@ Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, women
 # Output using ActiveRecord's count method to ensure 3 stores in database
 puts "Number of stores: #{Store.count}"
 
-# Stores that only carry mens apparel: Instance variable for @mens_stores
+# Fetch Stores that only carry mens apparel: Instance variable for @mens_stores
 @mens_stores = Store.where(mens_apparel: true)
 
 # Loop through stores, output name and annual revenue
@@ -30,3 +30,11 @@ end
 
 # Gastown would be outputted too, but we deleted it in previous exercise, create again using this
 # Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# Fetch stores that carry womens clothing and generate less than 1 million
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+
+# Loop through and output
+@womens_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
